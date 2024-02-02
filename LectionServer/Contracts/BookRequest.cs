fe4ff9e7d046c25bb991b93a5a337913b08c5d3d
Aleksandr787 <shuverov.aleksandr@gmail.com>
2024-02-02 21:15:26 +0300

    Добавьте файлы проекта.

---
+++ LectionServer/Contracts/BookRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace LectionServer.Contracts;
+
+public record BookRequest
+{
+    public required string Author { get; init; }
+    public required string Name { get; init; }
+    public required string ImageUrl { get; init; }
+}
